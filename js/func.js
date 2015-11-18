35f46a63b1a3c9b01007563562cd8d6832430a6e
channing333 <channinglulingwei@gmail.com>
2015-11-18 22:21:27 +0800

    22:21 加上各頁跳轉

--- js/func.js
+++ js/func.js
@@ -1,3 +1,7 @@
 function page(){
     return window.location.href = '../html/register.html';              // The function returns the product of p1 and p2
 }
+
+function index(){
+    return window.location.href = '../html/index.html';
+}
